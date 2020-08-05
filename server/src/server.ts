af4bc97e08b40599dbbc0263e993220c79c85a5b
Luan Ferreira <luan.ferreira.mads@gmail.com>
2020-08-05 14:41:18 -0300

    Create Controllers & Add Routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,9 @@
 import express from "express";
+import routes from "./routes";
 
 const app = express();
 
 app.use(express.json());
-
-app.get("/users", (req, res) => {
-  return res.json({ message: "Hello World" });
-});
+app.use(routes);
 
 app.listen(3333, () => console.log("> [Server] - Server is running"));
