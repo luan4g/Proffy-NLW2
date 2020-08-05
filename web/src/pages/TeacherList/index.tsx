f308556ee8ceccb3c28ed8a35e0aeddbb8e6bf78
Luan Ferreira <luan.ferreira.mads@gmail.com>
2020-08-04 21:04:54 -0300

    Create Component TeacherItem

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
-
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import TeacherItem from '../../components/TeacherItem';
 
 import './styles.css';
 
@@ -29,33 +28,11 @@ function TeacherList() {
       </PageHeader>
 
       <main>
-        <article className="teacher-item">
-          <header>
-            <img src="https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4" alt="Luan"/>
-            <div>
-              <strong>Diego Fernandes</strong>
-              <span>Química</span>
-            </div>
-          </header>
-
-          <p>
-            Entusiasta das melhores tecnologias de química avançada.
-            <br /><br />
-            Apaixonado por explodir coisas em laboratórios e por mudar a vida das pessoas através da experiência. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-          </p>
-
-          <footer>
-            <p>
-              Preço/Hora
-              <strong>R$ 80,00</strong>
-            </p>
-
-            <button type="button">
-              <img src={whatsappIcon} alt="whatsapp Icon"/>
-              Entrar em contato
-            </button>
-          </footer>
-        </article>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
       </main>
     </div>
   );
